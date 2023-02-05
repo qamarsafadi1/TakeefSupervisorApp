979ffecbdef98ee9a8cf6b8d9c72d9451fb9dc0a
Qamar Safadi <qamar.elsafadi98@gmail.com>
2023-02-05 15:41:01 +0200

    feat: general pages

--- app/src/main/java/com/selsela/takeefapp/ui/aboutapp/AboutAppView.kt
+++ app/src/main/java/com/selsela/takeefapp/ui/aboutapp/AboutAppView.kt
@@ -16,16 +16,22 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.selsela.takeefapp.R
 import com.selsela.takeefapp.ui.common.AppLogoImage
 import com.selsela.takeefapp.ui.common.LottieAnimationView
+import com.selsela.takeefapp.ui.splash.ConfigViewModel
+import com.selsela.takeefapp.ui.splash.GeneralUiState
 import com.selsela.takeefapp.ui.theme.CardBg
 import com.selsela.takeefapp.ui.theme.LightBlue
 import com.selsela.takeefapp.ui.theme.Purple40
@@ -33,11 +39,47 @@ import com.selsela.takeefapp.ui.theme.TextColor
 import com.selsela.takeefapp.ui.theme.text12
 import com.selsela.takeefapp.ui.theme.text16Medium
 import com.selsela.takeefapp.ui.theme.text22Book
+import com.selsela.takeefapp.utils.Common
+import com.selsela.takeefapp.utils.Extensions
+import com.selsela.takeefapp.utils.Extensions.Companion.collectAsStateLifecycleAware
 import com.selsela.takeefapp.utils.ModifiersExtension.paddingTop
+import de.palm.composestateevents.EventEffect
+
 
 @Preview
 @Composable
-fun AboutAppView() {
+fun AboutAppView(
+    viewModel: ConfigViewModel = hiltViewModel()
+) {
+
+    val viewState: GeneralUiState by viewModel.uiState.collectAsStateLifecycleAware(GeneralUiState())
+    val context = LocalContext.current
+
+    AboutAppContent(viewState)
+    /**
+     * Handle Ui state from flow
+     */
+
+    LaunchedEffect(Unit) {
+        if (viewModel.isLoaded.not()) {
+            viewModel.getAboutApp()
+        }
+    }
+
+    EventEffect(
+        event = viewState.onFailure,
+        onConsumed = viewModel::onFailure
+    ) { error ->
+        Common.handleErrors(
+            error.responseMessage,
+            error.errors,
+            context
+        )
+    }
+}
+
+@Composable
+private fun AboutAppContent(viewState: GeneralUiState) {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -76,12 +118,12 @@ fun AboutAppView() {
                         .fillMaxWidth()
                 ) {
                     Text(
-                        text = stringResource(id = R.string.quality),
+                        text = stringResource(R.string.quality_1),
                         style = text22Book,
                         color = Purple40
                     )
                     Text(
-                        text = stringResource(id = R.string.fast),
+                        text = stringResource(R.string.fast),
                         style = text22Book,
                         color = TextColor,
                         modifier = Modifier
@@ -89,7 +131,7 @@ fun AboutAppView() {
                             .padding(start = 43.dp)
                     )
                     Text(
-                        text = stringResource(id = R.string.safety),
+                        text = stringResource(R.string.safety),
                         style = text22Book,
                         color = LightBlue,
                         modifier = Modifier
@@ -116,13 +158,13 @@ fun AboutAppView() {
                 ) {
 
                     Text(
-                        text = stringResource(id = R.string.about_app),
+                        text = stringResource(R.string.about_app),
                         style = text16Medium,
                         color = TextColor
                     )
 
                     Text(
-                        text = stringResource(id = R.string.temp_details),
+                        text = Extensions.bindHtml(viewState.aboutApp?.text ?: ""),
                         style = text12,
                         color = TextColor
                     )
--- app/src/main/java/com/selsela/takeefapp/ui/terms/TermsCondationsView.kt
+++ app/src/main/java/com/selsela/takeefapp/ui/terms/TermsCondationsView.kt
@@ -8,18 +8,59 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.selsela.takeefapp.R
+import com.selsela.takeefapp.ui.splash.ConfigViewModel
+import com.selsela.takeefapp.ui.splash.GeneralUiState
 import com.selsela.takeefapp.ui.theme.TextColor
 import com.selsela.takeefapp.ui.theme.text12
 import com.selsela.takeefapp.ui.theme.text16Medium
+import com.selsela.takeefapp.utils.Common
+import com.selsela.takeefapp.utils.Extensions
+import com.selsela.takeefapp.utils.Extensions.Companion.collectAsStateLifecycleAware
 import com.selsela.takeefapp.utils.ModifiersExtension.paddingTop
+import de.palm.composestateevents.EventEffect
 
 @Composable
-fun TermsView() {
+fun TermsView(
+    viewModel: ConfigViewModel = hiltViewModel()
+) {
+    val viewState: GeneralUiState by viewModel.uiState.collectAsStateLifecycleAware(GeneralUiState())
+    val context = LocalContext.current
+
+    TermContent(viewState)
+
+    /**
+     * Handle Ui state from flow
+     */
+
+    LaunchedEffect(Unit) {
+        if (viewModel.isLoaded.not()) {
+            viewModel.getTerms()
+        }
+    }
+
+    EventEffect(
+        event = viewState.onFailure,
+        onConsumed = viewModel::onFailure
+    ) { error ->
+        Common.handleErrors(
+            error.responseMessage,
+            error.errors,
+            context
+        )
+    }
+}
+
+@Composable
+private fun TermContent(viewState: GeneralUiState) {
     Column(
         Modifier
             .padding(top = 18.dp)
@@ -30,16 +71,18 @@ fun TermsView() {
     ) {
 
         Text(
-            text = stringResource(id = R.string.terms_confaitions),
+            text = stringResource(id = R.string.terms_condition),
             style = text16Medium,
             color = TextColor
         )
 
-        Text(
-            text = stringResource(id = R.string.temp_details),
-            style = text12,
-            color = TextColor,
-            modifier = Modifier.paddingTop(24)
-        )
+        viewState.term?.text?.let {
+            Text(
+                text = Extensions.bindHtml(it),
+                style = text12,
+                color = TextColor,
+                modifier = Modifier.paddingTop(24)
+            )
+        }
     }
 }
